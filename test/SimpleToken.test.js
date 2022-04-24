3242f3a06d50f26e7b917c2a6316655796172ae1
fdx00 <trevorcolohan@gmail.com>
2022-04-24 20:05:18 +0100

    init hash

--- test/SimpleToken.test.js
+++ test/SimpleToken.test.js
@@ -1,8 +1,8 @@
 const assert = require('assert/strict');
 const { ethers } = require("hardhat");
 
-describe("MiniToken", function () {
-  let MiniToken;
+describe("SimpleToken", function () {
+  let SimpleToken;
 
   let deployer, anotherPerson;
 
@@ -11,43 +11,43 @@ describe("MiniToken", function () {
   });
 
   before('deploy the contract', async function () {
-    const factory = await ethers.getContractFactory("MiniToken");
-    MiniToken = await factory.deploy();
+    const factory = await ethers.getContractFactory("SimpleToken");
+    SimpleToken = await factory.deploy();
 
-    await MiniToken.deployed();
+    await SimpleToken.deployed();
   });
 
   describe('before a transfer is made', function () {
     it('should show that the deployer address has 1000 tokens', async function () {
       assert.equal(
-        (await MiniToken.balanceOf(deployer.address)).toString(),
+        (await SimpleToken.balanceOf(deployer.address)).toString(),
         '1000'
       );
     });
 
     it('should show that another address has no tokens', async function () {
       assert.equal(
-        (await MiniToken.balanceOf(anotherPerson.address)).toString(),
+        (await SimpleToken.balanceOf(anotherPerson.address)).toString(),
         '0'
       );
     });
 
     describe('when tokens are transfered', function () {
       before('transfer tokens', async function () {
-        const tx = await MiniToken.transfer(anotherPerson.address, 500);
+        const tx = await SimpleToken.transfer(anotherPerson.address, 500);
         await tx.wait();
       });
 
       it('should show that the deployer address has 500 tokens', async function () {
         assert.equal(
-          (await MiniToken.balanceOf(deployer.address)).toString(),
+          (await SimpleToken.balanceOf(deployer.address)).toString(),
           '500'
         );
       });
 
       it('should show that another address has 500 tokens', async function () {
         assert.equal(
-          (await MiniToken.balanceOf(anotherPerson.address)).toString(),
+          (await SimpleToken.balanceOf(anotherPerson.address)).toString(),
           '500'
         );
       });
